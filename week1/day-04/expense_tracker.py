83f598f49c20777d1269f243b20b188ee19e9970
jit0341 <jit0341@github.com>
2025-10-04 18:35:35 +0530

    Modified expense tracker

--- week1/day-04/expense_tracker.py
+++ week1/day-04/expense_tracker.py
@@ -15,14 +15,14 @@ while True:
         description = input("Enter description:")
         expense = {"amount":amount,"category":category,"description":description}
         expenses.append(expense)
-        print(f"Added ₹{amount} exoense to {category}")
+        print(f"Added ₹{amount} expense to {category}")
 
 
     elif choice == "2":
         if len(expenses) == 0:
             print("No expenses recorded yet!")
         else:
-            print("\n====All expensese====")
+            print("\n====All expenses====")
             for expense in expenses:
                 print(f"Amount: ₹{expense['amount']}")
                 print(f"Category: {expense['category']}")
@@ -43,9 +43,9 @@ while True:
     elif choice == "4":
         with open("expenses.txt","w") as file:
             for expense in expenses:
-                line = f"{amount}|{category["Food","Rent","Transport"]}"
+                line = f"{amount}|{category}|{description}"
                 file.write(line)
-                print("Expense savedbin a file")
+                print("Expense saved in a file")
         print("Goodbye!")
         break
     else:
