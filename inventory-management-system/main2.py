a7a2cd05bcd3e58b60e837614c8ce14de0efe3c0
jit0341 <jitendrablog6@gmail.com>
2025-10-21 02:41:49 +0000

    steps3 started.

--- inventory-management-system/main2.py
+++ inventory-management-system/main2.py
@@ -1,10 +1,72 @@
 # =========Configuration=============
-inventory
-_file = inventory.txt
-inventory = {}
+inventories_file = "inventories.txt"
+inventories = {}
 SEPARATOR = "|"
 
 #===========FILE OPERATION=========
-def load_inventory():
-    global inventory
+def load_inventories():
+    global inventories
+    
+    inventories = {}
+    try:
+        with open("inventories_file","r") as file:
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue
+                
+                parts = line.split(SEPARATOR)
+                
+                if len(parts) == 3:
+                    product_name, price, quantity = parts
+                    
+                    inventories[product_name.title()] = {
+                        "price": price,
+                        "quantity": quantity
+                    }
+                    
+        print("Inventories loaded successfully from inventories.txt.")
+    except FileNotFoundError:
+        print("No invetories file found! Starting fresh.")
+    except Exception as e:
+        print(f"An error occured during loading: {e}")
+        
+        inventories = {}
+        
+def save_inventories():
+    try:
+        with open("inventories_file","w") as file:
+            for product_name,details in inventories.items():
+                line = f"{product_name}{SEPARATOR}{details['price']}{SEPARATOR}{details['quantity']}\n"
+                file.write(line)
+        print("\nInventories saved successfully to inventories.txt.")
+                
+    except Exception as e:
+        print(f"Error saving inventories: {e}")
+        
+# ============= CRUD Operations =============
+
+def add_inventories():
+    global inventories
+    print("\n------Add new inventories------")
+    
+    product_name = input("Enter product name:").strip().title()
+        
+    if not product_name:
+        print("Product name cannot be empty.")
+        return
+    if product_name in inventories:
+        print(f"Inventory '{product_name}' already exists.")
+        return
+    price = input("Enter product price:").strip() or "N/A"
+    quantity = input("Enter quantity").strip() or "N/A"
+    
+    inventories[product_name] = {
+        "price": price,
+        "quantity": quantity
+    }
+    print(f"Inventories '{product_name}' added successfully.")
+    
+def view_all_inventories():
+    
     
