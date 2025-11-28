73795b64bf01afaf0fbeb4fa246a0cf864978114
jit0341 <jit0341@github.com>
2025-11-28 15:58:03 +0530

    Modified panda_practice1.py.

--- pandas/panda_practice1.py
+++ pandas/panda_practice1.py
@@ -1,7 +1,7 @@
 # Month Extraction
 import pandas as pd
 
-dates = pd.date_range('2025-01-01', periods = 5, freq = '15D')
+dates = pd.date_range('2025-01-01', periods = 12, freq = 'MS')
 print("Original Dates:")
 print(dates)
 
@@ -13,7 +13,7 @@ print(months)
 #Use in Dataframe
 test_df = pd.DataFrame({
     'Date': dates,
-    'Amount': [1000,2000,1500,3000,2500]
+    'Amount': [1000,2000,1500,3000,2500,500,4000,3500,1200,2200,3200,3500]
     })
 test_df['Month'] = test_df['Date'].dt.strftime('%B')
 print("\nDataFrame:")
