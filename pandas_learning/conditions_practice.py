c0a78c05833c2cc5535838df7ac671b6360761af
jit0341 <jit0341@github.com>
2025-12-04 20:42:26 +0530

    Second exercise done.

--- pandas_learning/conditions_practice.py
+++ pandas_learning/conditions_practice.py
@@ -21,3 +21,25 @@ print("\nExperienced Middle earners (Age 25-32 AND Salary 30000-50000):")
 target = df[(df['Age'] >= 25) & (df['Age'] <= 32) & (df['Salary'] >= 30000) & (df['Salary'] <= 50000)]
 print(target)
 
+df = pd.DataFrame({
+    'Name': ['Sneha','Sakshi','Ansh','Ritika','Arya'],
+    'Age': [25,20,28,26,35],
+    'Salary': [35000,40000,50000,60000,30000]
+    })
+
+# Example 1.1: AND condition
+print("Young and High earners (Age <30 AND Salary > 35000):")
+young_high = df[(df['Age'] < 30) & (df['Salary'] >= 35000)]
+print(young_high)
+
+# Example 2.1: OR condition
+print("\nSenior OR Rich (Age >30 OR Salary >50000):")
+senior_rich = df[(df['Age'] >30) | (df['Salary'] >50000)]
+print(senior_rich)
+
+# Example 3.1: Complex conditiom
+print("\nExperienced Middle Earners (Age 25-32 AND Salary 30000-50000):")
+
+target = df[(df['Age'] >= 25) & (df['Age'] <= 32) & (df['Salary'] >= 30000) & (df['Salary'] <= 50000)]
+print(target)
+        
