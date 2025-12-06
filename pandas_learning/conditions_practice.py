3b8b8ca12ce46ec2c8b20970f9d28c32500ce0a6
jit0341 <jit0341@github.com>
2025-12-06 22:35:48 +0530

    Modified for 3rd round of practice

--- pandas_learning/conditions_practice.py
+++ pandas_learning/conditions_practice.py
@@ -20,14 +20,18 @@ print(senior_rich)
 print("\nExperienced Middle earners (Age 25-32 AND Salary 30000-50000):")
 target = df[(df['Age'] >= 25) & (df['Age'] <= 32) & (df['Salary'] >= 30000) & (df['Salary'] <= 50000)]
 print(target)
+print("="*30)
+print("\nShow DataFrame:")
+print(df)
 
 df = pd.DataFrame({
     'Name': ['Sneha','Sakshi','Ansh','Ritika','Arya'],
-    'Age': [25,20,28,26,35],
+    'Age': [20,30,20,35,28],
     'Salary': [35000,40000,50000,60000,30000]
     })
 
 # Example 1.1: AND condition
+print("\n------Practice 2--------")
 print("Young and High earners (Age <30 AND Salary > 35000):")
 young_high = df[(df['Age'] < 30) & (df['Salary'] >= 35000)]
 print(young_high)
@@ -42,4 +46,30 @@ print("\nExperienced Middle Earners (Age 25-32 AND Salary 30000-50000):")
 
 target = df[(df['Age'] >= 25) & (df['Age'] <= 32) & (df['Salary'] >= 30000) & (df['Salary'] <= 50000)]
 print(target)
-        
+print("="*30)
+print("\nShow DataFrame:")
+print(df)
+
+print("\n------Example 3----------------")
+df = pd.DataFrame({
+    'Name': ['Manan','Manogya','Swastik','Zoya','Mannat'],
+    'Age': [29,22,30,21,28],
+    'Salary': [100000,40000,35000,60000,90000]
+    })
+ # Example 1.3: AND condition
+print("Young and High earners (Age <30 AND Salary >35000):")
+young_high = df[(df['Age'] < 30) & (df['Salary'] >=35000)]
+print(young_high)
+
+# Example 2.3: OR condition
+print("\nSenior OR Rich (Age >30 OR Salary >50000):")
+senior_rich = df[(df['Age'] >30) | (df['Salary'] >50000)]
+print(senior_rich)
+
+# Example 3.3: Complex condition
+print("\nExperienced Middle Earmers (Age 25-32 AND Salary 30000-80000):")
+target = df[(df['Age'] >= 25 ) & (df['Age'] <= 32) & (df['Salary'] >= 30000) & (df['Salary'] <= 80000)]
+print(target)
+print("="*30)
+print("\nShow DataFrame:")
+print(df)
